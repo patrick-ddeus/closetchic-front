95e8553402fabc9d371f821e651465d50f5aab35
Lucas Neris <lucasnerism@gmail.com>
2023-04-28 20:42:53 -0300

    Cart page layout done

--- src/pages/CartPage/style.jsx
+++ src/pages/CartPage/style.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import colors from "../../constants/colors.js";
 
 export const ContainerCartPage = styled.div`
   display: flex;
@@ -13,11 +14,11 @@ export const ContainerCartPage = styled.div`
 export const ContainerLeft = styled.div`
   width: 59%;
   border-right: 1px solid #D9D9D9;
-  padding: 80px 46px 0px 31px;  
+  padding: 40px 46px 0px 31px;  
 `
 export const ContainerRight = styled.div`
   width: 41%;
-  padding: 80px 72px 0px 47px;
+  padding: 40px 72px 0px 47px;
 `
 export const SectionBreak = styled.div`
   width: 100%;
@@ -25,7 +26,7 @@ export const SectionBreak = styled.div`
   background-color: #D9D9D9;
 `
 export const TableDescriptions = styled.div`
-  padding: 0 22px 0 234px;  
+  padding: 0 22px 0 0px;  
   height: 50px;
   display: flex;
   justify-content: space-between;
@@ -37,13 +38,58 @@ export const TableDescriptions = styled.div`
   }
 `
 export const ContainerCart = styled.div`
-  padding-top: 38px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  padding-top: 38px;  
+  max-height: 468px;
+  overflow-y: auto;  
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  ::-webkit-scrollbar{
+    display: none;
+  }
 `
 export const Product = styled.div`
   height: 174px;
+  padding-right: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: 'Raleway';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  p{
+    width: 80px;
+  }
+  img{
+    width: 172px;
+    height: 174px;
+  }
+  span{
+    font-weight: 500;
+    width: 80px;
+    text-align: end;
+  }
+  strong p{
+    font-weight: 700;
+    width: 120px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    :hover{
+      overflow: visible;
+      white-space: break-spaces;
+    }
+  }
+  input{
+    width: 63px;
+    height: 38px;
+    background: #FFFFFF;
+    border: 1px solid #EAEAEA;
+    text-align: center;
+  }
+  && ~ &&{
+    margin-top: 25px;
+  }
 `
 export const ContainerSubtotal = styled.div`
   height: 50px;
@@ -57,10 +103,65 @@ export const ContainerSubtotal = styled.div`
   }
 `
 export const Coupon = styled.div`
+  height: 147px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   input{
-
+    width: 294px;
+    height: 43px;
+    background: #FFFFFF;
+    border: 1px solid #D9D9D9;
+    padding-left: 21px;
+    ::placeholder{
+      font-family: 'Raleway';
+      font-weight: 500;
+      font-size: 16px;
+      color: #B0B0B0;
+    }
   }
   button{
-    
+    width: 137px;
+    height: 43px;    
+    background: ${colors.primaryColor};
+    font-family: 'Raleway';    
+    font-weight: 600;
+    font-size: 14px;
+    color: #FFFFFF;
+    border: none;
+    cursor: pointer;
+  }
+`
+export const ContainerToCheckout = styled.div`
+  padding-top: 43px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  button{
+    width: 431px;
+    height: 57px;    
+    background: ${colors.primaryColor};
+    font-family: 'Raleway';    
+    font-weight: 600;
+    font-size: 18px;
+    color: #FFFFFF;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-top: 44px;
+  }
+`
+export const ContainerTotal = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  font-family: 'Raleway';
+  font-weight: 500;
+  font-size: 16px;
+  text-align: right;
+  p{
+    margin-bottom: 6px;
   }
 `
