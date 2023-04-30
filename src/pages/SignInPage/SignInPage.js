e09c059da1c35672c771fadba744c18485a32c60
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-04-29 22:34:37 -0300

    fix bug on signin page

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,69 +1,73 @@
-import { Link, useNavigate } from "react-router-dom"
-import { UserContext } from "../../contexts/userContext.js"
-import { useState, useContext, useRef } from "react"
-import axios from 'axios'
-import { BackgroundImage, FormContainer, InputContainer, Linked, SignInButton, SignInContainer, TextDiv } from "./styles.jsx"
-import { IoIosLock, IoMdMail } from "react-icons/io"
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../contexts/userContext.js";
+import { useState, useContext, useRef } from "react";
+import axios from 'axios';
+import { BackgroundImage, FormContainer, InputContainer, Linked, SignInButton, SignInContainer, TextDiv } from "./styles.jsx";
+import { IoIosLock, IoMdMail } from "react-icons/io";
 import { motion } from "framer-motion";
 
 export default function SignInPage() {
-    const navigate = useNavigate()
-    const url = process.env.REACT_APP_API_URL
-    const emailRef = useRef(null)
-    const passwordRef = useRef(null)
-    const [form, setForm] = useState({ email: "", password: "" })
-    const [isDisabled, setIsDisabled] = useState(false)
-    const { setToken, setName } = useContext(UserContext)
+    const navigate = useNavigate();
+    const url = process.env.REACT_APP_API_URL;
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const [form, setForm] = useState({ email: "", password: "" });
+    const [isDisabled, setIsDisabled] = useState(false);
+    const { setToken, setName } = useContext(UserContext);
     const [invalidInputs, setInvalidInputs] = useState({
         email: null,
         password: null
-      });
+    });
     function handleChange(event) {
+        setForm({ ...form, [event.target.name]: event.target.value });
         const { name, value } = event.target;
-        setForm({ ...form, [name]: value })
+        setForm({ ...form, [name]: value });
         if (value === "" || !event.target.validity.valid) {
-            switch(name){
-              case "email":
-                emailRef.current.focus();
-                break;
-              case "password":
-                passwordRef.current.focus();
-                break;
+            switch (name) {
+                case "email":
+                    emailRef.current.focus();
+                    break;
+                case "password":
+                    passwordRef.current.focus();
+                    break;
+                default:
+                    return;
             }
             // adiciona o input inválido ao estado
             setInvalidInputs((prevInvalidInputs) => ({
-              ...prevInvalidInputs,
-              [name]: true,
+                ...prevInvalidInputs,
+                [name]: true,
             }));
-          } else {
+        } else {
             // remove o input inválido do estado
             setInvalidInputs((prevInvalidInputs) => {
-              const { [name]: removedInput, ...rest } = prevInvalidInputs;
-              return rest;
+                const { [name]: removedInput, ...rest } = prevInvalidInputs;
+                return rest;
             });
-          }
+        }
     }
     function getInputStyle(inputName) {
         return invalidInputs[inputName] ? { border: "2px solid red" } : {};
-      }
+    }
 
     function handleBlur(event) {
         const { name, value } = event.target;
         if (value === "" || !event.target.validity.valid) {
-        
-          // adiciona o input inválido ao estado
-          setInvalidInputs((prevInvalidInputs) => ({
-            ...prevInvalidInputs,
-            [name]: true,
-          }));
+
+            // adiciona o input inválido ao estado
+            setInvalidInputs((prevInvalidInputs) => ({
+                ...prevInvalidInputs,
+                [name]: true,
+            }));
         } else {
-          // remove o input inválido do estado
-          setInvalidInputs((prevInvalidInputs) => {
-            const { [name]: removedInput, ...rest } = prevInvalidInputs;
-            return rest;
-          });
+            // remove o input inválido do estado
+            setInvalidInputs((prevInvalidInputs) => {
+                const { [name]: removedInput, ...rest } = prevInvalidInputs;
+                return rest;
+            });
         }
-      }
+    }
+
     function signIn(e) {
         e.preventDefault();
         const promise = axios.post(`http://localhost:8000/sign-in/`, form);
@@ -109,7 +113,15 @@ export default function SignInPage() {
                     <TextDiv>Faça Login</TextDiv>
                     <FormContainer onSubmit={signIn}>
                         <InputContainer>
-                            <input placeholder="E-mail" type="email" disabled={isDisabled} name={"email"} onChange={handleChange} />
+                            <input
+                                ref={emailRef}
+                                placeholder="E-mail"
+                                type="email"
+                                disabled={isDisabled}
+                                name={"email"}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                style={getInputStyle("email")} />
                             <IoMdMail
                                 color={'#00000'}
                                 title={"person"}
@@ -118,7 +130,16 @@ export default function SignInPage() {
                             />
                         </InputContainer>
                         <InputContainer>
-                            <input placeholder="Senha" type="password" autoComplete="new-password" disabled={isDisabled} name={"password"} onChange={handleChange} />
+                            <input
+                                ref={passwordRef}
+                                placeholder="Senha"
+                                type="password"
+                                autoComplete="new-password"
+                                disabled={isDisabled}
+                                name={"password"}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                style={getInputStyle("password")} />
                             <IoIosLock
                                 color={'#00000'}
                                 title={"person"}
@@ -139,4 +160,3 @@ export default function SignInPage() {
         </BackgroundImage>
     );
 }
-
