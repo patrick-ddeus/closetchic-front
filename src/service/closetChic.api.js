df47aa32a481a1b1214c2a24c168da0c59ceb27f
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-05-02 00:54:38 -0300

    changes axios instance url

--- src/service/closetChic.api.js
+++ src/service/closetChic.api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: process.env.REACT_APP_API_URL || "https://closetchic-api.onrender.com/",
     headers: { "Content-Type": "application/json" },
 });
 
